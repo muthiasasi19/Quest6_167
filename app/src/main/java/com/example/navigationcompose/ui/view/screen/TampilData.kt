13334302b7feff08dd3a9cd373b6bd621f1c93a5
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-11-21 22:07:20 +0700

    Menambahkan tampilan kolom dan row untuk header di TampilView

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/TampilData.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/TampilData.kt
@@ -1,6 +1,24 @@
 package com.example.navigationcompose.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigationcompose.R
 import com.example.navigationcompose.model.Mahasiswa
 import com.example.navigationcompose.model.RencanaStudi
 
@@ -12,5 +30,37 @@ fun TampilView(
     onbackbuttonClicked: () -> Unit,
     onResetButtonClicked: () -> Unit,
 ) {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.primary)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+        Row (
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "umy",
+                modifier = Modifier.size(45.dp)
+            )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column {
+                Text(
+                    text = "Data KRS Mahasiswa",
+                    color = Color.White,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.White,
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }
+    }
 }
+
