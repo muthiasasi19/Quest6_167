d058ca3f4d642ccf756dc1598907e24916bfa2db
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-11-21 21:41:34 +0700

    Menambahkan variabel state untuk dropdown dan checkbox serta mendefinisikan list data

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
@@ -1,6 +1,7 @@
 package com.example.navigationcompose.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import com.example.navigationcompose.model.Mahasiswa
 
 @Composable
@@ -9,5 +10,16 @@ fun RencanaStudyView(
     onSubmitButton: (MuutableList<String>) -> Unit,
     onbackbuttonClicked: () -> Unit,
 ) {
-
+    var chosenDropdown by remember {}
+    muteableStateOf(
+        ""
+    )
 }
+
+var checked by remember { muteableStateOf(false) }
+var pilihKelas by remember {
+    muteableStateOf("")
+}
+
+var listData: MuteableList<String> = muteableListOf(chosenDropdown,
+    pilihanKelas)
