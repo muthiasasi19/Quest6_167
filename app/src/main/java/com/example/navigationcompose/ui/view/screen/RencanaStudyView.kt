25498b29fdc5a6d776484f0e82bee7349c6856d3
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-11-22 06:41:54 +0700

    Menambahkan fungsi saveDataKRS untuk menyimpan data KRS dan memperbarui state dengan nama mata kuliah dan kelas

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
@@ -19,6 +19,13 @@ class RencanaStudyViewModel : ViewModel() {
     fun setKelas(kelasPilihan: String) {
         _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
     }
+    fun saveDataKRS(ls: MutableList<String>) {
+        _krsState.update { status ->
+            status.copy(
+                namaMK = ls[0],
+                kelas = ls[1]
+            ) }
+    }
 }
 
 
