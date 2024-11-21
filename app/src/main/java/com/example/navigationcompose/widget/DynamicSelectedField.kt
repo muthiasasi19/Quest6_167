6f7dfbfdcdd7fbfd6fde01225ee98dc2cc2babfc
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-11-22 06:49:34 +0700

    Menambahkan menu dropdown

--- app/src/main/java/com/example/navigationcompose/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/navigationcompose/widget/DynamicSelectedField.kt
@@ -1,2 +1,30 @@
 package com.example.navigationcompose.widget
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import java.lang.reflect.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DynamicSelectedField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+   // modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {expanded = !expanded},
+        //modifier = Modifier
+    ) {
+
+    }
+
+
+}
