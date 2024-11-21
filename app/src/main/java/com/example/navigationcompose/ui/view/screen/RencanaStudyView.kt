76a750a67e78d2e55f66e3c33e276aeb492df258
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-11-21 21:36:58 +0700

    Menambahkan tampilan RencanaStudyView dengan parameter Mahasiswa

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/RencanaStudyView.kt
@@ -1,2 +1,13 @@
 package com.example.navigationcompose.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import com.example.navigationcompose.model.Mahasiswa
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButton: (MuutableList<String>) -> Unit,
+    onbackbuttonClicked: () -> Unit,
+) {
+
+}
