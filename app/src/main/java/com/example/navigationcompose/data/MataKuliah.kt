565de714413cfc20949e714a18eeb95e23ec569f
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-11-22 07:28:10 +0700

    Menambahkan objek MataKuliah dengan daftar pilihan mata kuliah

--- app/src/main/java/com/example/navigationcompose/data/MataKuliah.kt
+++ app/src/main/java/com/example/navigationcompose/data/MataKuliah.kt
@@ -1,4 +1,13 @@
 package com.example.navigationcompose.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pemrograman Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasaran Buatan",
+        "Computer Vision"
+    )
+
 }
