2d61caa7b8f91caaa4cb89b110306162d7af696f
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-11-22 07:47:02 +0700

    Menambahkan tampilan RencanaStudyView dengan pilihan dropdown dan data kelas

--- app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.navigationcompose.ui.view.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.example.navigationcompose.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
