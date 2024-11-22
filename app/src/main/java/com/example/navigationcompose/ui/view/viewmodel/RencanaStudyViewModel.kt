1e8acf8dd245073b276dc0c2fc6d01dd5e7c7174
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-11-22 07:14:19 +0700

    Menambahkan RencanaStudyViewModel dengan pengelolaan state KRS dan fungsi untuk mengatur mata kuliah yang dipilih

---
+++ app/src/main/java/com/example/navigationcompose/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.navigationcompose.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.navigationcompose.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudyViewModel : ViewModel() {
+    private  val _krsState = MutableStateFlow(RencanaStudi())
+
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah (mkPilihan: String) {
+        _krsState.update { stateMk -> stateMk.copy(namaMK = mkPilihan) }
+    }
+
+}
